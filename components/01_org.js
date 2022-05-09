79f14ef9cb55a7f196e8da3d158597ceebcd525b
Dianne <mdianne.gabriel@gmail.com>
2022-05-09 15:47:28 -0400

    Create new directories for organization

--- components/01_org.js
+++ components/01_org.js
@@ -8,10 +8,20 @@ client.setEnvironment(platformClient.PureCloudRegionHosts.ca_central_1);
 const CLIENT_ID = process.env.GENESYS_CLOUD_CLIENT_ID;
 const CLIENT_SECRET = process.env.GENESYS_CLOUD_CLIENT_SECRET;
 
+let organizationApi = new platformClient.OrganizationApi();
+
 // Authenticate with Genesys Cloud
 client.loginClientCredentialsGrant(CLIENT_ID, CLIENT_SECRET)
 .then(() => {
-  console.log("Authentication Success!")
+  console.log("Authentication Success!");
+  organizationApi.getOrganizationsMe()
+  .then((data) => {
+    console.log(`getOrganizationsMe success! data: ${JSON.stringify(data, null, 2)}`);
+  })
+  .catch((err) => {
+    console.log('There was a failure calling getOrganizationsMe');
+    console.error(err);
+  });
 })
 .catch((err) => {
   console.error(err);
