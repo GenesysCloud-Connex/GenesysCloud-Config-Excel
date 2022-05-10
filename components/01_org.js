1150c8225ab2a3d63fc8f25b8571f67e6f99316e
Dianne <mdianne.gabriel@gmail.com>
2022-05-10 15:02:41 -0400

    Model the json from the API to fit the xlsx requirement

--- components/01_org.js
+++ components/01_org.js
@@ -1,5 +1,6 @@
 // Obtain a reference to the platformClient object
 require('dotenv').config();
+const xlsx = require("json-as-xlsx");
 const platformClient = require('purecloud-platform-client-v2');
 const client = platformClient.ApiClient.instance;
 
@@ -17,6 +18,30 @@ client.loginClientCredentialsGrant(CLIENT_ID, CLIENT_SECRET)
   organizationApi.getOrganizationsMe()
   .then((data) => {
     console.log(`getOrganizationsMe success! data: ${JSON.stringify(data, null, 2)}`);
+    let dataArr = [];
+    let obj = {
+      sheet: "Organization_V2",
+      columns: [],
+      content: [],
+    };
+    let col = [];
+
+    Object.keys(data).forEach(function (key) {
+      let dataColumn = { label: key, value: key }; // Top level data
+      col.push(dataColumn);
+      return col;
+    });
+    obj.columns = col;
+    obj.content = [data];
+    dataArr.push(obj);
+    console.log(obj);
+
+    const settings = {
+      fileName: "Sample", // Name of the resulting spreadsheet
+      extraLength: 3, // A bigger number means that columns will be wider
+      writeOptions: {}, // Style options from https://github.com/SheetJS/sheetjs#writing-options
+    };
+    xlsx(dataArr, settings);
   })
   .catch((err) => {
     console.log('There was a failure calling getOrganizationsMe');
